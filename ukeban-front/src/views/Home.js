fb9370b8922fbf82005cbf7a273fd011e6afc8e8
lindong ye <lindong_ye@163.com>
2022-04-12 22:55:41 -0700

    navlink

--- ukeban-front/src/views/Home.js
+++ ukeban-front/src/views/Home.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div>
+        Home
+        <h1>less</h1>
+        <h1>mafanl</h1>
+        </div>
     )
   }
 }
